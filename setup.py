b669cb4674fed23f2dcb49a9bad1c05e28a76ddc
danshorstein <dshorstein@gmail.com>
2016-11-21 16:10:44 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['re', 'pandas'],
+    install_requires=['re', 'pandas']
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
