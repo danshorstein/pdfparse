5bfc042df5c52a034b85fcb1258736ae11861212
danshorstein <dshorstein@gmail.com>
2016-11-21 16:09:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,15 +61,15 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='parse','pdf parser','parses','tables'
+    keywords='parse'
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    #   py_modules=["my_module"],
+    # py_modules=["my_module"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
