4d1cf19ac1797828b54f2862b427694f4fc27fa4
danshorstein <dshorstein@gmail.com>
2016-11-21 16:49:41 -0500

    UPDATE

--- parsable/__init__.py
+++ parsable/__init__.py
@@ -1,1 +1,14 @@
+"""
+Parsable
+~~~~~~~~~~~~~~~~~~~~~
+Parsable is a text parser written in Python, for extracting table-like
+PDF data from PDF files 
 
+Basic parsable usage:
+   >>> import parsable
+   >>> tbd.....
+
+   
+"""
+
+print('WORKING ON THIS!!!')
