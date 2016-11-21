1dfd10e3f279f0cd4dc75ef921c59c74f33f67a3
danshorstein <dshorstein@gmail.com>
2016-11-21 16:09:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     ],
 
     # What does your project relate to?
-    keywords='parse'
+    keywords=['parse', 'tables', 'pdf']
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
