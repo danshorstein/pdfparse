ba033d7a29cfdae4ae197acd09f23a660301fae5
Dan Shorstein <dshorstein@gmail.com>
2016-12-06 16:50:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -57,15 +57,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['parsable',
-             'api',
-             'file_io',
-             'regex_stuff',
-             'tools'],
+    # packages=['parsable'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    # py_modules=["parsable.py"],
+    py_modules=["parsable.py"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
