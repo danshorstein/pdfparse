a84132b9f0fdb32d4554253838bde25356e2a197
Dan Shorstein <dshorstein@gmail.com>
2016-12-06 16:49:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -58,10 +58,10 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=['parsable',
-             '.api',
-             '.file_io',
-             '.regex_stuff',
-             '.tools'],
+             'api',
+             'file_io',
+             'regex_stuff',
+             'tools'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
