c25fc684d8bd861d37c4b38ca10fc00f7e02ca4f
Dan Shorstein <dshorstein@gmail.com>
2016-12-06 16:52:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/danshorstein/parsicle',
+    url='https://github.com/danshorstein/parsable',
 
     # Author details
     author='Daniel Shorstein',
@@ -57,11 +57,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=['parsable'],
+    packages=['parsable'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=["parsable.py"],
+    # py_modules=["parsable.py"],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
@@ -74,7 +74,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'parsicle=parsicle:main',
+            'parsable=parsable:main',
         ],
     },
 )
